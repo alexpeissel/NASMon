b5e055e06fa944b79809623725fa86875afa85ed
Alex Peissel <alexpeissel@googlemail.com>
2018-08-06 22:55:31 +0100

    Added display test function

--- Monitor.py
+++ Monitor.py
@@ -17,11 +17,12 @@ import glob
 import random
 from logzero import logger
 from Microcontroller import Microcontroller
-from Pages import Page
+from Page import Page
 
 
 class Monitor:
     """Main class"""
+
     def __init__(self, page_directory, serial_port, baud_rate):
         """Loads pages from page_directory and connects to microcontroller.
         Args:
@@ -43,12 +44,17 @@ class Monitor:
             logger.error("Exiting")
             exit(1)
 
+        self._test_displays(5)
+        self.mc.clear_displays()
+
         while True:
             if self.mc.is_requesting_data():
                 self.mc.clear_displays()
+
                 current_page = self.pages[self.current_page_index]
-                logger.info("Displaying page: %s", self.pages[self.current_page_index].name)
-                self._show_page(current_page)
+                logger.info("Displaying page: %s", current_page.name)
+                self._show_page(self.pages[self.current_page_index])
+
                 if self.current_page_index == len(self.pages) - 1:
                     self.current_page_index = 0
                 else:
@@ -76,17 +82,46 @@ class Monitor:
     def _show_page(self, page):
         logger.debug("Page data: %s", page.get_text_components())
         for text_data in page.get_text_components():
-            self.mc.display_text(text_data['text'], text_data['x'], text_data['y'], text_data['size'])
+            self.mc.display_text(text_data.get("text"),
+                                 text_data.get("x"),
+                                 text_data.get("y"),
+                                 text_data.get("size")
+                                 )
 
-        # TODO: handle bitmaps
+        # # TODO: handle bitmaps
         # for bmp_data in page.get_bmp_components():
-        #     self.mc.display_bmp(bmp_data)
+        #     bmp = bytes([0b11111111])
+        #     print bmp
+        #     self.mc.display_bmp(bmp,
+        #                         1,
+        #                         1,
+        #                         8,
+        #                         2
+        #                         )
 
         for bargraph_data in page.get_bargraph_components():
             self.mc.display_bargraph(bargraph_data.get("graph_value"))
 
-    def generate_random_string(self, length):
-        return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+    def _generate_random_text_component(self, length):
+        text_component = {
+            "text": ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length)),
+            "x": 0,
+            "y": 0,
+            "size": 1
+        }
 
-    def generate_random_graph(self):
+        return text_component
+
+    def _generate_random_graph_component(self):
         return ''.join(random.choice('rgyo') for _ in range(24))
+
+    def _test_displays(self, number_of_text_components):
+        for i in xrange(number_of_text_components):
+            random_text_component = self._generate_random_text_component(10)
+            self.mc.clear_displays()
+            self.mc.display_text(random_text_component.get("text"),
+                                 random_text_component.get("x"),
+                                 random_text_component.get("y"),
+                                 random_text_component.get("size")
+                                 )
+            self.mc.display_bargraph(self._generate_random_graph_component())
