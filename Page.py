a0872e1a47f9675a88477cc003cc90428e687d60
Alex Peissel <alexpeissel@googlemail.com>
2018-09-18 17:57:58 +0100

    Updated pages to use component objects

--- Page.py
+++ Page.py
@@ -15,127 +15,85 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 import yaml
-import subprocess
-from string import Template
 from logzero import logger
+
+from TextComponent import TextComponent
+from ImageComponent import ImageComponent
+from BargraphComponent import BargraphComponent
+from CommandComponent import CommandComponent
 
 
 class Page:
     def __init__(self, page_file):
-        self.name = "NASMon page"
+        self.name = ""
         self.text_components = []
         self.bmp_components = []
         self.bargraph_components = []
         self._command_components = []
         self._command_outputs = {}
+
         self._parse_page_file(page_file)
 
     def get_text_components(self):
-        return self._get_interpolated_components("text", self.text_components, "text")
+        """ Return a list of interpolated text dicts """
+        self._update_components(self.text_components)
+        return self.text_components
 
     def get_bmp_components(self):
+        """ Return a list of interpolated bmp dicts """
+        # TODO: fix the update_components method for images
+        #self._update_components(self.bmp_components)
         return self.bmp_components
 
     def get_bargraph_components(self):
-        return self._get_interpolated_components("bargraph", self.bargraph_components, "graph_value")
+        """ Return a list of interpolated bargraph dicts """
+        self._update_components(self.bargraph_components)
+        return self.bargraph_components
 
-    def _update(self):
-        for command_component in self._command_components:
-            logger.debug("Updating var: %s", command_component.get("variable_identifier"))
-            result = self._run_command(command_component.get("command"))
-            logger.debug("Updating result to: %s", result)
-            self._command_outputs[command_component.get("variable_identifier")] = result
+    def update_command_outputs(self):
+        """ Runs each command component and adds the result to the self._command_outputs dict, e.g. {'cpu': '32'} """
+        for component in self._command_components:
+            component.update()
+            self._command_outputs[component.variable_identifier] = component.result
 
-    def _get_interpolated_components(self, component_type, source_component_list, field_to_update):
-        self._update()
-        interpolated_components = []
-        for component in source_component_list:
-            updated_component = {}
-            updated_component.update(component)
-
-            interpolated_value = ""
-
-            if component_type == "text":
-                interpolated_value = Template(updated_component.get(field_to_update)).safe_substitute(
-                    self._command_outputs)
-            elif component_type == "bmp":
-                # Nothing to interpolate yet
-                pass
-            elif component_type == "bargraph":
-                interpolated_value = self._convert_int_to_bargraph(
-                    Template(updated_component.get("graph_value")).safe_substitute(self._command_outputs))
-            else:
-                logger.error("component_type is invalid, returning empty string")
-
-            updated_component[field_to_update] = interpolated_value
-            interpolated_components.append(updated_component)
-        return interpolated_components
-
-    def _convert_bitmap_to_1_bit(self):
-        pass
-
-    def _convert_int_to_bargraph(self, value, green_threshold=0, yellow_threshold=50, red_threshold=75):
-        bargraph_data = ["o"] * 24
-        for i in xrange(0, len(bargraph_data)):
-            if int(float(value)) > ((i + 1) * (100.0 / 24)):
-                if (i + 1) * (100.0 / 24) >= red_threshold:
-                    bargraph_data[i] = 'r'
-                elif (i + 1) * (100.0 / 24) >= yellow_threshold:
-                    bargraph_data[i] = 'y'
-                elif (i + 1) * (100.0 / 24) >= green_threshold:
-                    bargraph_data[i] = 'g'
-                else:
-                    bargraph_data[i] = 'o'
-
-        return "".join(reversed(bargraph_data))
+    def _update_components(self, component_list):
+        """ Call update method on any list of components """
+        for component in component_list:
+            component.update(self._command_outputs)
 
     def _parse_page_file(self, page_file):
+        """ Parse YAML page files and set public attributes """
+        # Parse YAML
         with open(page_file) as stream:
             try:
-                page_file_data = yaml.safe_load(stream)
-                self._parse_page_data(page_file_data)
+                data = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 logger.error(exc)
 
-    def _parse_page_data(self, data):
-        self.name = data.get("name")
+        # Set the page name
+        self.name = data.get("name", "NASMon page")
 
-        for text_component in data.get("text"):
-            self.text_components.append(text_component)
+        # Create TextComponents
+        for text_component in data.get("text", []):
+            self.text_components.append(TextComponent(**text_component))
 
-        for bmp_component in data.get("bmp"):
-            self.bmp_components.append(bmp_component)
+        # Create BMPComponents
+        for bmp_component in data.get("bmp", []):
+            logger.debug("Creating initial image component")
+            initial_component = ImageComponent(**bmp_component)
 
-        if len(data.get("graph")) > 1:
-            logger.warn("Only 1 graph value is allowed, taking the first value only.")
+            logger.debug("Appending %i sub-images to image component list", len(initial_component.sub_image_components))
+            for image_component in initial_component.sub_image_components:
+                self.bmp_components.append(image_component)
 
-        bargraph_data = data.get("graph")[0]
-        output = {
-            'graph_value': bargraph_data.get("graph_value"),
-            'green_threshold': bargraph_data.get("green_threshold"),
-            'yellow_threshold': bargraph_data.get("yellow_threshold"),
-            'red_threshold': bargraph_data.get("red_threshold")
-        }
+        # Create one BargraphComponent
+        for bargraph_component in data.get("bargraph", []):
+            if len(data.get("bargraph", [])) > 1:
+                logger.warn("Only 1 graph value is allowed, taking the first value only.")
 
-        self.bargraph_components.append(output)
+            self.bargraph_components.append(BargraphComponent(**bargraph_component))
+            break
 
+        # Create CommandComponents
         for command_component in data.get("commands"):
-            self._command_components.append({
-                'variable_identifier': command_component.get("variable_identifier").replace("$", ""),
-                'command': command_component.get("command"),
-                'update': True,
-            })
-
-            self._command_outputs[command_component.get("variable_identifier").replace("$", "")] = None
-
-    def _run_command(self, command):
-        logger.debug("Running command: %s", command)
-        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        return_value = p.wait()
-        result = None
-        if return_value is 0:
-            result = "".join(p.stdout.readlines()).rstrip()
-            logger.debug("Command result: %s", result)
-        else:
-            logger.warn("Command returned a non 0 value")
-        return result
+            self._command_components.append(CommandComponent(**command_component))
